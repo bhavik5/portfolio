5c1ab3a513bd9a048fc898447606770fe013606f
bhavik5 <s.bhavik.v.5@gmail.com>
2020-03-07 01:19:50 +0530

    Updates

---
+++ precache-manifest.fb77b1d72775ddaaaa875387fb000fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4412326760d54662443a289a0c4866",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "758c8e2584b40a229be4",
+    "url": "/portfolio/static/css/main.b7de0216.chunk.css"
+  },
+  {
+    "revision": "8775563f26236cae13d4",
+    "url": "/portfolio/static/js/2.aabefd6c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.aabefd6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "758c8e2584b40a229be4",
+    "url": "/portfolio/static/js/main.32dc2421.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
