6c62569d3f90eccf55162a4ff12cf29361195fbf
bhavik5 <s.bhavik.v.5@gmail.com>
2020-03-07 01:41:12 +0530

    Set Mobile Menu

--- src/components/Main.js
+++ src/components/Main.js
@@ -37,6 +37,7 @@ class Main extends React.Component {
               <ul className="anchor_nav">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
+                <li><a href="#technicalCompetencies">Technologies</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
               </ul>
